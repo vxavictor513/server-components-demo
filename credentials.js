aa1af841049da6d0917b6f49a2f09ca78445c0cb
Wai Loon <wai.loon.theng@accenture.com>
2021-02-26 11:46:25 +0800

    Updated credentials

--- credentials.js
+++ credentials.js
@@ -2,6 +2,6 @@ module.exports = {
   host: 'wl-postgres.cjrj1rklvi3m.ap-southeast-1.rds.amazonaws.com',
   database: 'notesapi',
   user: 'postgres',
-  password: #{DB_PASSWORD}#,
+  password: "#{DB_PASSWORD}#",
   port: '15432',
 };
